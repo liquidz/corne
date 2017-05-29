180b952df248d83ebb0d3347b84618ecb7326de9
liquidz <liquidz.uo@gmail.com>
2017-05-29 09:03:42 +0000

    :cat:

--- src/cola.lisp
+++ src/cola.lisp
@@ -1,10 +1,15 @@
 (in-package :cl-user)
 (defpackage cola
-  (:use :cl))
+  (:use :cl)
+  (:export
+    :option
+    :command)
+  )
 (in-package :cola)
 
 (defclass option ()
-  ((short       :initform () :initarg :short)
+  ((name        :initform "" :initarg :name :reader get-name)
+   (short       :initform () :initarg :short)
    (long        :initform () :initarg :long)
    (help        :initform "" :initarg :help)
    (takes-value :initform () :initarg :takes-value)))
@@ -15,6 +20,16 @@
    (version     :initform "" :initarg :version)
    (subcommands :initform () :initarg :subcommands)
    (options     :initform () :initarg :options)))
+
+(defstruct result
+  subcommand
+  options
+  arguments)
+
+(defun add-result-option! (result option)
+  (let ((o (result-options result)))
+    (setf (result-options result)
+          (nconc o (list option)))))
 
 (defun command (&rest args)
   (apply #'make-instance 'command args))
@@ -35,9 +50,25 @@
   (let ((ls (slot-value cmd 'options)))
     (find opt ls :test #'equivalent)))
 
-  (setq cmd _sample_command_))
-  (setq args '("-h"))
-(defmethod parse ((cmd command) args &optional (result ()))
+;(setq cmd _sample_command_)
+;(setq args '("-h"))
+;
+;(let* ((args '("-h" "foo"))
+;       (arg (first args))
+;       (result (make-result))
+;       (cmd _sample_command_)
+;       (c (find-command cmd arg))
+;       (o (find-option cmd arg)))
+;  (if (slot-value o 'takes-value)
+;    t
+;    (progn (setf (result-options result)
+;
+;                 ))
+;    ()
+;    )
+;  )
+
+(defmethod parse ((cmd command) args &optional (result (make-result)))
   (unless args
     result
     (let* ((arg (first args))
@@ -49,9 +80,12 @@
           )
         (o
           "kiteru"
-          (if (slot-value o 'takes-value)
-            (parse cmd (cddr args))
-            (parse cmd (cdr args)))
+          ;(if (slot-value o 'takes-value)
+          ;  (progn
+          ;    (add-result-option! result )
+          ;    )
+          ;  (parse cmd (cddr args))
+          ;  (parse cmd (cdr args)))
           )
         (t
           )
@@ -60,13 +94,18 @@
     )
   )
 
+
 (setq _sample_command_
   (command :name "sample" :about "test" :version "1.0"
            :subcommands (list
                           (command :name "foo" :about "foobar" :version "1.0"
                                    :options (list (option :short "v" :long "verbose" :help "baz"))))
-           :options (list (option :short "h" :long "help" :help "print help"))
+           :options (list
+                      (option :name "help" :short "h" :long "help" :help "print help")
+                      (option :name "foo" :short "f" :long "foo" :takes-value t :help "bar")
+                      )
            ))
+
 
 
 
--- t/cola.lisp
+++ t/cola.lisp
@@ -7,7 +7,9 @@
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :cola)' in your Lisp.
 
-(plan nil)
+(plan 1)
+
+(ok (typep (cola:option :name "help" :short "h" :long "help" :help "print help") 'command))
 
 ;; blah blah blah.
 
